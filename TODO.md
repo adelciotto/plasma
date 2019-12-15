98f3126db7e1b6cf2bdcbb301e787f766619f2a3
Anthony Del Ciotto <anthony.delciotto@greensync.com.au>
2019-12-15 13:57:30 +1100

    Final software plasma effect and palette
    Achieved the final look I want for the software plasma effect.
    I experimented with using multiple plasma buffers, but, didn't
    really get the look I wanted. So keeping it simple with one
    palette and plasma.

--- TODO.md
+++ TODO.md
@@ -1,6 +1,9 @@
 # TODO
 
+- [ ] Implement slower dynamic plasma
+  - [  ] Try and optimize
+  - [  ] Implement fast version in GLSL
+- [ ] Draw palettes to image on disk
 - [ ] Interactivity
     - Maybe ripple as mouse moves
     - Click for bigger force
-- [ ] Multiple plasmas
--- plasma.c
+++ plasma.c
@@ -14,14 +14,6 @@
 #define LogError(...) SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 #define LogInfo(...) SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 
-typedef enum {
-  RED_GREEN_BLACK_PALETTE,
-  RED_BLACK_BLUE_PALETTE,
-  BLACK_GREEN_BLUE_PALETTE,
-  HSV_HUE_PALETTE,
-  HSV_HUE_LIGHTNESS_PALETTE
-} PaletteType;
-
 SDL_DisplayMode displayMode;
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
@@ -33,7 +25,6 @@ Uint32 palette[PALETTE_SIZE];
 int width = DEFAULT_WIDTH;
 int height = DEFAULT_HEIGHT;
 int fullscreen = 0;
-const PaletteType paletteType = RED_GREEN_BLACK_PALETTE;
 
 double Min(double value, double min) { return value < min ? value : min; }
 
@@ -41,64 +32,6 @@ int Get1DArrayIndex(int x, int y, int width) { return (y * width) + x; }
 
 Uint32 RGBToUint32(Uint8 r, Uint8 g, Uint8 b) {
   return (Uint32)((r << 16) + (g << 8) + b);
-}
-
-Uint32 HSVToUint32(int h, int s, int v) {
-  double hue = h / 256.0;
-  double saturation = s / 256.0;
-  double value = v / 256.0;
-  double r, g, b;
-
-  if (saturation == 0) {
-    r = g = b = value;
-  } else {
-    // Bring the hue to a number between 0 to 6. Better for calculations.
-    hue *= 6;
-
-    int i = (int)floor(hue);
-
-    // The fractional part of the hue.
-    double f = hue - i;
-    double p = value * (1.0 - saturation);
-    double q = value * (1.0 - (saturation * f));
-    double t = value * (1.0 - (saturation * (1.0 - f)));
-
-    switch (i) {
-    case 0:
-      r = value;
-      g = t;
-      b = p;
-      break;
-    case 1:
-      r = q;
-      g = value;
-      b = p;
-      break;
-    case 2:
-      r = p;
-      g = value;
-      b = t;
-      break;
-    case 3:
-      r = p;
-      g = q;
-      b = value;
-      break;
-    case 4:
-      r = t;
-      g = p;
-      b = value;
-      break;
-    case 5:
-      r = value;
-      g = p;
-      b = q;
-      break;
-    }
-  }
-
-  return RGBToUint32((Uint8)(r * 255.0), (Uint8)(g * 255.0),
-                     (Uint8)(b * 255.0));
 }
 
 double GetElapsedTimeSecs(Uint64 start, Uint64 end) {
@@ -138,6 +71,8 @@ int InitSDL(void) {
     SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
   }
 
+  SDL_ShowCursor(SDL_DISABLE);
+
   renderer = SDL_CreateRenderer(
       window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   if (renderer == NULL) {
@@ -156,58 +91,11 @@ int InitSDL(void) {
   return 0;
 }
 
-void InitRedGreenBlackPalette() {
-  for (int x = 0; x < PALETTE_SIZE; x++) {
-    Uint8 r = (Uint8)Min(128.0 + 128 * sin(M_PI * x / 32.0), 255);
-    Uint8 g = (Uint8)Min(128.0 + 128 * sin(M_PI * x / 128.0), 255);
-    palette[x] = RGBToUint32(r, g, 0);
-  }
-}
-
-void InitRedBlackBluePalette() {
-  for (int x = 0; x < PALETTE_SIZE; x++) {
-    Uint8 r = (Uint8)Min(128.0 + 128 * sin(M_PI * x / 32.0), 255);
-    Uint8 b = (Uint8)Min(128.0 + 128 * sin(M_PI * x / 128.0), 255);
-    palette[x] = RGBToUint32(r, 0, b);
-  }
-}
-
-void InitBlackGreenBluePalette() {
-  for (int x = 0; x < PALETTE_SIZE; x++) {
-    Uint8 g = (Uint8)Min(128.0 + 128 * sin(M_PI * x / 32.0), 255);
-    Uint8 b = (Uint8)Min(128.0 + 128 * sin(M_PI * x / 128.0), 255);
-    palette[x] = RGBToUint32(0, g, b);
-  }
-}
-
-void InitHSVHuePalette() {
-  for (int x = 0; x < PALETTE_SIZE; x++) {
-    palette[x] = HSVToUint32(x, 255, 255);
-  }
-}
-
-void InitHSVHueAndLightnessPalette() {
-  for (int x = 0; x < PALETTE_SIZE; x++) {
-    palette[x] = HSVToUint32(x, 255, x);
-  }
-}
-
 void InitPalette(void) {
-  switch (paletteType) {
-  case RED_GREEN_BLACK_PALETTE:
-    InitRedGreenBlackPalette();
-    break;
-  case RED_BLACK_BLUE_PALETTE:
-    InitRedBlackBluePalette();
-    break;
-  case BLACK_GREEN_BLUE_PALETTE:
-    InitBlackGreenBluePalette();
-    break;
-  case HSV_HUE_PALETTE:
-    InitHSVHuePalette();
-  case HSV_HUE_LIGHTNESS_PALETTE:
-    InitHSVHueAndLightnessPalette();
-    break;
+  for (int x = 0; x < PALETTE_SIZE; x++) {
+    Uint8 r = (Uint8)Min(128.0 + 128 * sin(M_PI * x / 32.0), 255);
+    Uint8 b = (Uint8)Min(128.0 + 128 * sin(M_PI * x / 64.0), 255);
+    palette[x] = RGBToUint32(r, 0, b);
   }
 }
 
@@ -218,16 +106,19 @@ void InitPlasma(void) {
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
       double color = 128.0 + (128.0 * sin(x / 16.0));
-      color += 128.0 + (128.0 * sin(y / 32.0));
+      color += 128.0 + (128.0 * sin(y / 8.0));
+      color += 128.0 + (128.0 * sin((x + y) / 16.0));
       color +=
           128.0 +
           (128.0 * sin(sqrt((double)((x - halfWidth) * (x - halfWidth) +
                                      (y - halfHeight) * (y - halfHeight))) /
                        8.0));
+      // Uncomment for some weird shit
+      // color += 128.0 + (128.0 * sin((x * y) / 128.0));
       color += 128.0 + (128.0 * sin(sqrt((double)(x * x + y * y)) / 8.0));
 
       int index = Get1DArrayIndex(x, y, width);
-      plasmaBuffer[index] = (Uint32)color / 6;
+      plasmaBuffer[index] = (Uint32)color / 8;
     }
   }
 }
@@ -337,9 +228,9 @@ int main(int argc, char *argv[]) {
     Uint64 endCounter = SDL_GetPerformanceCounter();
 
     SDL_UpdateTexture(texture, NULL, pixelBuffer, width * sizeof(*pixelBuffer));
+    SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, texture, NULL, NULL);
     SDL_RenderPresent(renderer);
-    SDL_RenderClear(renderer);
 
     double msPerFrame = GetElapsedTimeMs(lastCounter, endCounter);
     double fps = (double)SDL_GetPerformanceFrequency() /
